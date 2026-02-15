56da1b864e3c247ed62654e8db01aec6b7ec830b
Ajay Kumar <Ajay847303@gmail.com>
2026-02-16 02:43:55 +0530

    utilites config

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,38 @@
+const asyncHandler = (requestHandler) => {
+
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+export { asyncHandler}
+
+
+
+
+
+
+//rapar fn bana rhe hai jo har ek route ke liye use hoga taki har ek route me try catch na likhna pade aur error handling ek jaisi ho jaye BY TRY CATCH METHOD  
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+// try {
+//   await fn(req,res,next)
+
+//   } catch (error) {
+
+//   res.status(500).json({
+//     success: false,
+//     message: error.message
+//   })
+//   }
+// }
