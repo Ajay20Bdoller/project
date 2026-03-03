d771d1ea513122bf82eb14386e3300d8ceb3cce6
Ajay Kumar <Ajay847303@gmail.com>
2026-03-03 19:15:43 +0530

    feat: implement user routes for registration, login, logout, and token refresh

---
+++ src/routes/user.routes.js
@@ -0,0 +1,49 @@
+import { Router } from "express";
+import { loginUser, registerUser, logoutUser, refreshAccessToken} from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+
+
+
+
+
+
+const router = Router();
+
+router.route("/register").post(
+  
+  //going to use middleware here to 
+    upload.fields([
+    {
+      name: "avatar",
+      maxCount:1
+    },
+    {
+      name: "coverImage",
+      maxCount:1
+    }
+
+  ]),
+//here
+  
+  registerUser
+)
+
+
+
+
+router.route("/login").post(loginUser)
+
+
+
+
+//secure routes
+router.route("/logout").post(verifyJWT, logoutUser)
+
+
+router.route("/refresh-token").post(refreshAccessToken)
+
+export default router;
+
+
