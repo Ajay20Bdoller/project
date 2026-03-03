da5362e0662b9b565d7dbd08b60cd8eeb3cfd5fb
Ajay Kumar <Ajay847303@gmail.com>
2026-03-04 01:33:01 +0530

    feat: add aggregation pipeline to fetch user channel profile with subscription details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -404,6 +404,81 @@ return res
 })
 
 
+///AGGREGATION PIPELINE***************/////
+
+const getUserChannelProfile= asyncHandler(async(req, res)=>{
+const {username}= req.params
+
+if(!username?.trim()){
+  throw new APIError(400, "username is missing")
+}
+
+const channel=  await User.aggregate([
+  {
+    $match :{
+    username:username?.toLowerCase()
+  }
+  },{
+    $lookup:{
+      from:"subscriptions",
+      localField:"_id",
+      foreignField:"channel",
+      as: "subscriber"
+    }
+  },
+  {
+     $lookup:{
+      from:"subscriptions",
+      localField:"_id",
+      foreignField:"subscriber",
+      as: "subscribedTo"
+    }
+  },{
+    $addFields:{
+      subscribersCount:{
+        $size: "$subscribers"
+      },
+      channelsSubscribedToCount:{
+        $size: "$subscribedTo"
+      },
+      isSubscribed:{
+        $cond:{
+          if:{$in:[req.user?._id, "$subscribers.subscriber"],
+            then: true,
+            else :false
+          }
+        }
+      }
+    }
+  },{
+    $project:{
+      username: 1,
+      fullname: 1,
+      subscribersCount:1,
+      channelsSubscribedToCount:1,
+      isSubscribed:1,
+      avatar:1,
+      coverImage:1,
+      email:1
+
+
+
+    }
+  }
+])
+
+if(!channel?.length){
+  throw new APIError(404, "channel does not exit")
+}
+return res
+.stat(200)
+.json(
+  new APIResponse(200, "user channel fetches successfully")
+)
+
+})
+
+
 
 export {registerUser,
    loginUser, 
@@ -414,7 +489,8 @@ export {registerUser,
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    
+    getUserChannelProfile
+
 
 
 
